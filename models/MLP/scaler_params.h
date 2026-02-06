4d9b720a8d3d705713d878f620f897bc188081ec
Jonas Souza <jonassouza871@hotmail.com>
2026-02-06 13:25:44 -0300

    adiciona arquitetura GRU

---
+++ models/MLP/scaler_params.h
@@ -0,0 +1,23 @@
+// Scaler parameters for normalization
+// Auto-generated file - Do not edit manually
+
+#ifndef SCALER_PARAMS_H
+#define SCALER_PARAMS_H
+
+// Mean values
+const float scaler_mean[] = {
+    20.276101f,  // Temp_AHT20_C
+    66.718630f,  // Umid_AHT20_pct
+    21.759548f,  // Temp_BMP280_C
+    918.015285f  // Press_BMP280_hPa
+};
+
+// Scale values
+const float scaler_scale[] = {
+    3.294128f,  // Temp_AHT20_C
+    13.480684f,  // Umid_AHT20_pct
+    3.191886f,  // Temp_BMP280_C
+    2.106629f  // Press_BMP280_hPa
+};
+
+#endif // SCALER_PARAMS_H
